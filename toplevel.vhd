800899211d83364e4b5b0c4e3201c2866b9a6c1a
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-08-14 22:45:55 -0700

    Update toplevel.vhd

--- toplevel.vhd
+++ toplevel.vhd
@@ -12,6 +12,9 @@ ARCHITECTURE behavior OF toplevel IS
 TYPE state IS (IDLE, RECEIVE, SEND);
 SIGNAL currentState, nextState : state;
 
+TYPE print IS (INPUT, BASE10, BASE16, ENTER, FEED);
+SIGNAL currLine, nextLine, prevLine : print;
+
 CONSTANT CR : STD_LOGIC_VECTOR := x"0D"; --Carriage Return
 CONSTANT LF : STD_LOGIC_VECTOR := x"0A"; --Line Feed
 CONSTANT BS : STD_LOGIC_VECTOR := x"08"; --Backspace
@@ -28,6 +31,9 @@ SIGNAL tx_valid : STD_LOGIC;
 
 SIGNAL dataString : STRING (16 DOWNTO 1);
 SIGNAL dataLogic : STD_LOGIC_VECTOR (127 DOWNTO 0);
+
+SIGNAL ASCII : STD_LOGIC_VECTOR (7 DOWNTO 0);
+SIGNAL HEX : STD_LOGIC_VECTOR (7 DOWNTO 0);
 
 COMPONENT UART_RX IS
     PORT(clk : IN  STD_LOGIC;
@@ -47,6 +53,13 @@ COMPONENT UART_TX IS
           tx_OUT : OUT STD_LOGIC);
 END COMPONENT;
 
+COMPONENT conv IS
+    PORT(clk : IN STD_LOGIC;
+         char : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+         ascii, hex : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+        );
+END COMPONENT;
+
 IMPURE FUNCTION STR2SLV (str : STRING) RETURN STD_LOGIC_VECTOR IS
     VARIABLE data : STD_LOGIC_VECTOR(str'LENGTH * 8 - 1 DOWNTO 0) ;
     BEGIN
@@ -59,6 +72,7 @@ END FUNCTION;
 BEGIN
     uartrx : UART_RX PORT MAP (clk => clk, reset => RST, rx_IN => RX, rx_valid => rx_valid, rx_data => rx_data);
     uarttx : UART_TX PORT MAP (clk => clk, reset => RST, tx_valid => tx_valid, tx_data => tx_data, tx_ready => tx_ready, tx_OUT => TX);
+    change : conv PORT MAP (clk => clk, char => rx_data, ascii => ASCII, hex => HEX);
 
     PROCESS(ALL)
     BEGIN
@@ -71,7 +85,6 @@ BEGIN
                     nextState <= SEND;
             END IF;
             WHEN SEND => tx_valid <= '1';
-                tx_data <= rx_data;
                 IF tx_ready = '1' AND tx_valid <= '1' AND RX = '1' THEN
                     tx_valid <= '0';
                     nextState <= IDLE;
@@ -83,10 +96,44 @@ BEGIN
     PROCESS(ALL)
     BEGIN
         IF RISING_EDGE(clk) THEN
-            IF RX = '0' THEN
+            CASE currLine IS
+            WHEN INPUT => IF RX = '0' THEN
                 dataString <= "Input: ";
                 dataLogic <= STR2SLV(dataString);
+                FOR i IN 16 DOWNTO 1 LOOP
+                    tx_data <= dataLogic(i * 8 - 1 DOWNTO i * 8 - 9);
+                END LOOP;
+                tx_data <= rx_data;
+                prevLine <= INPUT;
+                nextLine <= ENTER;
             END IF;
+            WHEN BASE10 => dataString <= "ASCII: ";
+                dataLogic <= STR2SLV(dataString);
+                FOR i IN 16 DOWNTO 1 LOOP
+                    tx_data <= dataLogic(i * 8 - 1 DOWNTO i * 8 - 9);
+                END LOOP;
+                tx_data <= ASCII;
+                prevLine <= BASE10;
+                nextLine <= ENTER;
+            WHEN BASE16 => dataString <= "Hex: 0x";
+                dataLogic <= STR2SLV(dataString);
+                FOR i IN 16 DOWNTO 1 LOOP
+                    tx_data <= dataLogic(i * 8 - 1 DOWNTO i * 8 - 9);
+                END LOOP;
+                tx_data <= HEX;
+                prevLine <= BASE16;
+                nextLine <= ENTER;
+            WHEN ENTER => tx_data <= CR;
+                nextLine <= FEED;
+            WHEN FEED => tx_data <= LF;
+                IF prevLine = INPUT THEN
+                    nextLine <= BASE10;
+                ELSIF prevLine = BASE10 THEN
+                    nextLine <= BASE16;
+                ELSE
+                    nextLine <= INPUT;
+                END IF;
+            END CASE;
         END IF;
     END PROCESS;
 
@@ -94,6 +141,7 @@ BEGIN
     BEGIN
         IF RISING_EDGE(clk) THEN
             currentState <= nextState;
+            currLine <= nextLine;
         END IF;
     END PROCESS;
 END ARCHITECTURE;
