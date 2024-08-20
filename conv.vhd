13870825d6cfab55f69a07484a036502076e6082
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-08-19 21:38:58 -0700

    Update conv.vhd

--- conv.vhd
+++ conv.vhd
@@ -1,40 +1,35 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.MATH_REAL.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 USE IEEE.NUMERIC_STD_UNSIGNED.ALL;
 
 ENTITY conv IS
     PORT(clk : IN STD_LOGIC;
          char : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-         ascii, hex : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+         ascii, hexLow, hexHigh : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')
         );
 END ENTITY;
 
 ARCHITECTURE behavior OF conv IS
-SIGNAL flag : STD_LOGIC := '1';
-SIGNAL temp : STD_LOGIC_VECTOR (7 DOWNTO 0);
+SIGNAL temp : INTEGER;
+
 BEGIN
     PROCESS(ALL)
     BEGIN
         IF RISING_EDGE(clk) THEN
-            FOR i IN 0 TO 1 LOOP
-                hex <= TO_STDLOGICVECTOR(48, 8) + char((7 - 4 * i) DOWNTO (4 - 4 * i)) WHEN char((7 - 4 * i) DOWNTO (4 - 4 * i)) <= 9 ELSE TO_STDLOGICVECTOR(55, 8) + char((7 - 4 * i) DOWNTO (4 - 4 * i));
-            END LOOP;
+            hexLow <= char(7 DOWNTO 4) + TO_STDLOGICVECTOR(48, 8) WHEN char(7 DOWNTO 4) <= 9 ELSE char(7 DOWNTO 4) + TO_STDLOGICVECTOR(55, 8);
+            hexHigh <= char(3 DOWNTO 0) + TO_STDLOGICVECTOR(48, 8) WHEN char(3 DOWNTO 0) <= 9 ELSE char(3 DOWNTO 0) + TO_STDLOGICVECTOR(55, 8);
         END IF;
     END PROCESS;
 
     PROCESS(ALL)
     BEGIN
         IF RISING_EDGE(clk) THEN
-            IF flag = '1' AND char > 0 THEN
-                temp <= char;
-                flag <= '0';
-            ELSE
-                WHILE temp >= 10 LOOP
-                    temp <= temp / 10;
-                    temp <= temp MOD 10;
-                END LOOP;
-            END IF;
-            ascii <= temp + TO_STDLOGICVECTOR(48, 8);
+            FOR i IN 7 DOWNTO 0 LOOP
+                temp <= 1 WHEN char(i) ELSE 0;
+                ascii <= ascii + STD_LOGIC_VECTOR(TO_UNSIGNED(temp * (2 ** i), 8));
+            END LOOP;
         END IF;
     END PROCESS;
 END ARCHITECTURE;
