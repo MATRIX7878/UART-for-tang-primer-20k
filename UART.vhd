0f646e7e0d22dd359105ec4c3d03cee2898fcdec
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-09-19 12:30:00 -0700

    Update UART.vhd

--- UART.vhd
+++ UART.vhd
@@ -6,18 +6,18 @@ ENTITY UART_TX IS
     PORT (clk : IN  STD_LOGIC;
           reset : IN  STD_LOGIC;
           tx_valid : IN STD_LOGIC;
-          tx_data : IN  STD_LOGIC_VECTOR (7 downto 0);
+          tx_data : IN  STD_LOGIC_VECTOR (7 DOWNTO 0);
           tx_ready : OUT STD_LOGIC;
           tx_OUT : OUT STD_LOGIC);
 END UART_TX;
 
 ARCHITECTURE Behavior OF UART_TX IS
 TYPE state IS (IDLE, START, SEND, STOP);
-SIGNAL currentState, nextState : state;
+SIGNAL currentState : state;
 
 CONSTANT BAUD : STD_LOGIC_VECTOR (7 DOWNTO 0) := d"234";
 
-SIGNAL bits : INTEGER RANGE 7 DOWNTO 0:= 7;
+SIGNAL bits : INTEGER RANGE 7 DOWNTO 0 := 7;
 SIGNAL counter : STD_LOGIC_VECTOR (7 DOWNTO 0) := (OTHERS => '0');
 
 BEGIN
@@ -26,22 +26,22 @@ BEGIN
         IF RISING_EDGE(clk) THEN
             IF NOT reset THEN
                 CASE currentState IS
-                WHEN IDLE => IF tx_valid = '1'  THEN
+                WHEN IDLE => IF tx_valid = '1' THEN
                     tx_ready <= '0';
                     tx_OUT <= '1';
-                    nextState <= START;
+                    currentState <= START;
                 END IF;
                 WHEN START => IF counter = BAUD THEN
                     tx_OUT <= '0';
                     counter <= (OTHERS => '0');
-                    nextState <= SEND;
+                    currentState <= SEND;
                 ELSE
                     counter <= counter + '1';
                 END IF;
                 WHEN SEND => IF counter = BAUD AND bits = 0 THEN
                     tx_OUT <= tx_data(bits);
                     counter <= (OTHERS => '0');
-                    nextState <= STOP;
+                    currentState <= STOP;
                 ELSIF counter = BAUD AND bits > 0 THEN
                     tx_OUT <= tx_data(bits);
                     counter <= (OTHERS => '0');
@@ -54,25 +54,22 @@ BEGIN
                     tx_ready <= '1';
                     tx_OUT <= '1';
                     counter <= (OTHERS => '0');
-                    nextState <= IDLE;
+                    currentState <= IDLE;
                 ELSE
                     counter <= counter + '1';
                 END IF;
                 END CASE;
+            ELSE
+                currentState <= IDLE;
+                counter <= (OTHERS => '0');
+                bits <= 7;
             END IF;
-        END IF;
-    END PROCESS;
-
-    PROCESS(clk)
-    BEGIN
-        IF RISING_EDGE(clk) THEN
-            currentState <= nextState;
         END IF;
     END PROCESS;
 END ARCHITECTURE;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD_UNSIGNED.ALL;
 
 ENTITY UART_RX IS
@@ -86,7 +83,7 @@ END UART_RX;
 
 ARCHITECTURE Behavior OF UART_RX IS
 TYPE state IS (IDLE, START, RECEIVE, STOP);
-SIGNAL currentState, nextState : state;
+SIGNAL currentState : state;
 
 CONSTANT BAUD : STD_LOGIC_VECTOR (7 DOWNTO 0) := d"234";
 
@@ -101,18 +98,18 @@ BEGIN
                 CASE currentState IS
                 WHEN IDLE => IF NOT rx_IN THEN
                     rx_valid <= '0';
-                    nextState <= START;
+                    currentState <= START;
                 END IF;
                 WHEN START => IF counter = BAUD / 2 THEN
                     counter <= (OTHERS => '0');
-                    nextState <= RECEIVE;
+                    currentState <= RECEIVE;
                 ELSE
                     counter <= counter + '1';
                 END IF;
                 WHEN RECEIVE => IF counter = BAUD AND bits = 0 THEN
                     rx_data(bits) <= rx_IN;
                     counter <= (OTHERS => '0');
-                    nextState <= STOP;
+                    currentState <= STOP;
                 ELSIF counter = BAUD AND bits > 0 THEN
                     rx_data(bits) <= rx_IN;
                     counter <= (OTHERS => '0');
@@ -124,19 +121,16 @@ BEGIN
                     bits <= 7;
                     rx_valid <= '1';
                     counter <= (OTHERS => '0');
-                    nextState <= IDLE;
+                    currentState <= IDLE;
                 ELSE
                     counter <= counter + '1';
                 END IF;
                 END CASE;
+            ELSE
+                currentState <= IDLE;
+                counter <= (OTHERS => '0');
+                bits <= 7;
             END IF;
-        END IF;
-    END PROCESS;
-
-    PROCESS(clk)
-    BEGIN
-        IF RISING_EDGE(clk) THEN
-            currentState <= nextState;
         END IF;
     END PROCESS;
 END ARCHITECTURE;
