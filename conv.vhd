3f008c724050a163ff692f3a95352bb79c8f1926
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-09-23 16:14:09 -0700

    Update conv.vhd

--- conv.vhd
+++ conv.vhd
@@ -1,18 +1,23 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.MATH_REAL.ALL;
-USE IEEE.NUMERIC_STD.ALL;
 USE IEEE.NUMERIC_STD_UNSIGNED.ALL;
 
 ENTITY conv IS
     PORT(clk : IN STD_LOGIC;
          char : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-         ascii, hexLow, hexHigh : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')
+         hund, tens, ones, hexLow, hexHigh : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')
         );
 END ENTITY;
 
 ARCHITECTURE behavior OF conv IS
-SIGNAL temp : INTEGER;
+TYPE state IS (START, ADD3, SHIFT, DONE);
+SIGNAL currentState : state;
+
+SIGNAL step : STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
+SIGNAL cache : STD_LOGIC_VECTOR (7 DOWNTO 0) := (OTHERS => '0');
+SIGNAL digits : STD_LOGIC_VECTOR (11 DOWNTO 0) := (OTHERS => '0');
+
+SIGNAL temp1, temp2, temp3 : STD_LOGIC_VECTOR (11 DOWNTO 0);
 
 BEGIN
     PROCESS(ALL)
@@ -26,10 +31,29 @@ BEGIN
     PROCESS(ALL)
     BEGIN
         IF RISING_EDGE(clk) THEN
-            FOR i IN 7 DOWNTO 0 LOOP
-                temp <= 1 WHEN char(i) ELSE 0;
-                ascii <= ascii + STD_LOGIC_VECTOR(TO_UNSIGNED(temp * (2 ** i), 8));
-            END LOOP;
+            CASE currentState IS
+            WHEN START => cache <= char;
+                step <= (OTHERS => '0');
+                digits <= (OTHERS => '0');
+                currentState <= ADD3;
+            WHEN ADD3 => temp1 <= TO_STDLOGICVECTOR(3, 12) WHEN digits(3 DOWNTO 0) >= 5 ELSE (OTHERS => '0');
+                temp2 <= TO_STDLOGICVECTOR(48, 12) WHEN digits(7 DOWNTO 4) >= 5 ELSE (OTHERS => '0');
+                temp3 <= TO_STDLOGICVECTOR(768, 12) WHEN digits(11 DOWNTO 8) >= 5 ELSE (OTHERS => '0');
+                digits <= digits + temp1 + temp2 + temp3;
+                currentState <= SHIFT;
+            WHEN SHIFT => digits <= digits(10 DOWNTO 0) & cache(7);
+                cache <= cache(6 DOWNTO 0) & '0';
+                IF step = 7 THEN
+                    currentState <= DONE;
+                ELSE
+                    step <= step + '1';
+                    currentState <= ADD3;
+                END IF;
+            WHEN DONE => hund <= TO_STDLOGICVECTOR(48, 8) + digits(11 DOWNTO 8);
+                tens <= TO_STDLOGICVECTOR(48, 8) + digits(7 DOWNTO 4);
+                ones <= TO_STDLOGICVECTOR(48, 8) + digits(3 DOWNTO 0);
+                currentState <= START;
+            END CASE;
         END IF;
     END PROCESS;
 END ARCHITECTURE;
