0981e57d57d6ed5b7bf7a95b257748bcf385fbfd
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-09-26 15:53:07 -0700

    Update conv.vhd

--- conv.vhd
+++ conv.vhd
@@ -13,13 +13,16 @@ ARCHITECTURE behavior OF conv IS
 TYPE state IS (START, ADD3, SHIFT, DONE);
 SIGNAL currentState : state;
 
-TYPE part IS (HUN, TEN, ONE);
+TYPE part IS (INTS, BASE, HUN, TEN, ONE);
 SIGNAL currentPart : part;
 
-SIGNAL int : INTEGER;
+TYPE bits IS ARRAY (0 TO 7) OF INTEGER;
+SIGNAL int : bits;
 
 SIGNAL step : STD_LOGIC_VECTOR (3 DOWNTO 0) := (OTHERS => '0');
-SIGNAL cache, downup : STD_LOGIC_VECTOR (7 DOWNTO 0) := (OTHERS => '0');
+--SIGNAL cache, downup : STD_LOGIC_VECTOR (7 DOWNTO 0) := (OTHERS => '0');
+SIGNAL cache : INTEGER RANGE 0 TO 127;
+SIGNAL downup : STD_LOGIC_VECTOR (7 DOWNTO 0);
 SIGNAL digits : STD_LOGIC_VECTOR (11 DOWNTO 0) := (OTHERS => '0');
 
 SIGNAL temp1, temp2, temp3 : STD_LOGIC_VECTOR (11 DOWNTO 0);
@@ -33,24 +36,17 @@ BEGIN
     RETURN switch;
 END FUNCTION;
 
-IMPURE FUNCTION POW (raise : INTEGER) RETURN INTEGER IS
-VARIABLE power : INTEGER := 1;
-BEGIN
-	IF raise = 0 THEN
-		power := 1;
-	ELSE
-		FOR i IN 1 TO raise LOOP
-			power := 2 * power;
-		END LOOP;
-	END IF;
-	RETURN power;
-END FUNCTION;
-
 BEGIN
     PROCESS(ALL)
     BEGIN
         IF RISING_EDGE(clk) THEN
-			downup <= UPDOWN(char);
+            downup <= UPDOWN(char);
+        END IF;
+    END PROCESS;
+
+    PROCESS(ALL)
+    BEGIN
+        IF RISING_EDGE(clk) THEN
             hexHigh <= downup(7 DOWNTO 4) + TO_STDLOGICVECTOR(48, 8) WHEN downup(7 DOWNTO 4) <= 9 ELSE downup(7 DOWNTO 4) + TO_STDLOGICVECTOR(55, 8);
             hexLow <= downup(3 DOWNTO 0) + TO_STDLOGICVECTOR(48, 8) WHEN downup(3 DOWNTO 0) <= 9 ELSE downup(3 DOWNTO 0) + TO_STDLOGICVECTOR(55, 8);
         END IF;
@@ -82,52 +78,54 @@ BEGIN
 --                ones <= TO_STDLOGICVECTOR(48, 8) + digits(3 DOWNTO 0);
 --                currentState <= START;
 --            END CASE;
-			FOR i IN 0 TO 7 LOOP
-				int <= 1 WHEN char(i) ELSE 0;
-				cache <= TO_STDLOGICVECTOR(int * POW(i), 8);
-			END LOOP;
 
 			CASE currentPart IS
-			WHEN HUN => IF cache > 99 THEN
-				hund <= TO_STDLOGICVECTOR(48, 8) + 1;
+            WHEN INTS => FOR i IN 0 TO 7 LOOP
+				int(i) <= 1 WHEN char(i) ELSE 0;
+			END LOOP;
+                currentPart <= BASE;
+            WHEN BASE => cache <= int(0) * 1 + int(1) * 2 + int(2) * 4 + int(3) * 8 + int(4) * 16 + int(5) * 32 + int(6) * 64;
+                currentPart <= HUN;
+			WHEN HUN => IF cache >= 100 THEN
+				hund <= TO_STDLOGICVECTOR(48, 8) + "1";
 				cache <= cache - 100;
 			ELSE
 				hund <= TO_STDLOGICVECTOR(48, 8);
 			END IF;
 			currentPart <= TEN;
 			WHEN TEN => IF cache >= 90 AND cache <= 99 THEN
-				tens <= TO_STDLOGICVECTOR(48, 8) + 9;
+				tens <= TO_STDLOGICVECTOR(48, 8) + d"9";
 				cache <= cache - 90;
 			ELSIF cache >= 80 AND cache <= 89 THEN
-				tens <= TO_STDLOGICVECTOR(48, 8) + 8;
+				tens <= TO_STDLOGICVECTOR(48, 8) + d"8";
 				cache <= cache - 80;
 			ELSIF cache >= 70 AND cache <= 79 THEN
-				tens <= TO_STDLOGICVECTOR(48, 8) + 7;
+				tens <= TO_STDLOGICVECTOR(48, 8) + d"7";
 				cache <= cache - 70;
 			ELSIF cache >= 60 AND cache <= 69 THEN
-				tens <= TO_STDLOGICVECTOR(48, 8) + 6;
+				tens <= TO_STDLOGICVECTOR(48, 8) + d"6";
 				cache <= cache - 60;
 			ELSIF cache >= 50 AND cache <= 59 THEN
-				tens <= TO_STDLOGICVECTOR(48, 8) + 5;
+				tens <= TO_STDLOGICVECTOR(48, 8) + d"5";
 				cache <= cache - 50;
 			ELSIF cache >= 40 AND cache <= 49 THEN
-				tens <= TO_STDLOGICVECTOR(48, 8) + 4;
+				tens <= TO_STDLOGICVECTOR(48, 8) + d"4";
 				cache <= cache - 40;
 			ELSIF cache >= 30 AND cache <= 39 THEN
-				tens <= TO_STDLOGICVECTOR(48, 8) + 3;
+				tens <= TO_STDLOGICVECTOR(48, 8) + d"3";
 				cache <= cache - 30;
 			ELSIF cache >= 20 AND cache <= 29 THEN
-				tens <= TO_STDLOGICVECTOR(48, 8) + 2;
+				tens <= TO_STDLOGICVECTOR(48, 8) + d"2";
 				cache <= cache - 20;
 			ELSIF cache >= 10 AND cache <= 19 THEN
-				tens <= TO_STDLOGICVECTOR(48, 8) + 1;
+				tens <= TO_STDLOGICVECTOR(48, 8) + "1";
 				cache <= cache - 10;
 			ELSE
 				tens <= TO_STDLOGICVECTOR(48, 8);
 			END IF;
 			currentPart <= ONE;
-			WHEN ONE =>	ones <= TO_STDLOGICVECTOR(48, 8) + cache;
-				currentPart <= HUN;
+			WHEN ONE =>	ones <= TO_STDLOGICVECTOR(48, 8) + TO_STDLOGICVECTOR(cache, 4);
+				currentPart <= BASE;
 			END CASE;
         END IF;
     END PROCESS;
